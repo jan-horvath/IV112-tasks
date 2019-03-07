1560a63b2d29fe47120ddd22d00de0a3ab433c55
Jan Horvath <433736@mail.muni.cz>
2019-03-07 09:23:22 +0100

    Task 2A almost finished (needs combinations with repetition)

--- Task_2/task2.cpp
+++ Task_2/task2.cpp
@@ -1,10 +1,11 @@
 
 #include <vector>
 #include <cassert>
+#include <iostream>
 
 using namespace std;
 
-vector<vector<char>> variations(const vector<char> &in, unsigned k) {
+vector<vector<char>> variations(const vector<char> &in, unsigned k, bool repetition = false) {
     assert(k <= in.size());
 
     vector<vector<char>> output;
@@ -18,7 +19,7 @@ vector<vector<char>> variations(const vector<char> &in, unsigned k) {
     for (size_t i = 0; i < in.size(); ++i) {
         auto copy = in;
         auto erased = copy[i];
-        copy.erase(copy.begin()+i);
+        if (!repetition) {copy.erase(copy.begin()+i);}
 
         auto toAppend = variations(copy, k-1);
         for (auto& item: toAppend) {
@@ -32,25 +33,34 @@ vector<vector<char>> variations(const vector<char> &in, unsigned k) {
 }
 
 vector<vector<char>> permutations(const vector<char> &in) {
-    vector<vector<char>> output;
-    if (in.size() == 1) {
-        return {in};
-    }
+    return variations(in, in.size());
+}
 
-    for (size_t i = 0; i < in.size(); ++i) {
-        auto copy = in;
-        auto erased = copy[i];
-        copy.erase(copy.begin()+i);
+vector<vector<char>> combinations(const vector<char> &input, unsigned k, bool repetition = false) {
+    if (!repetition && input.size() == k) {return {input};}
+    if (repetition && input.empty()) {return {};}
+    if (k == 0) {return {{}};}
 
-        auto toAppend = permutations(copy);
-        for (auto& item: toAppend) {
-            item.push_back(erased);
+    vector<vector<char>> merged;
+    unsigned maxIterations = repetition ? k : 2;
+
+    for (size_t i = 0; i < maxIterations; ++i) {
+        auto include_i_times = combinations({input.begin()+1, input.end()}, k-i, repetition);
+
+        for (auto& combination : include_i_times) {
+            for (size_t j = 0; j < i; ++j) {
+                combination.push_back(input[0]);
+            }
         }
-
-        output.insert(output.end(), toAppend.begin(), toAppend.end());
+        merged.insert(merged.end(), include_i_times.begin(), include_i_times.end());
     }
+    /*auto includeFirst = combinations({input.begin()+1, input.end()}, k-1, repetition);
+    auto excludeFirst = combinations({input.begin()+1, input.end()}, k, repetition);
 
-    return output;
+    merged = includeFirst;
+    merged.insert(merged.end(), excludeFirst.begin(), excludeFirst.end());*/
+
+    return merged;
 }
 
 int main() {
@@ -60,11 +70,11 @@ int main() {
         cout << endl;
     }*/
 
-    /*auto var = variations({'A', 'B', 'C', 'D', 'E'}, 2);
+    auto var = combinations({'A', 'B', 'C', 'D'}, 2, true);
     for (auto &item : var) {
         for (char c :item) {cout << c;}
         cout << endl;
-    }*/
+    }
 
     return 0;
 }
