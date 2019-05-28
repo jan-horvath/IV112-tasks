11d64652c396c4e17a0482e7ddc7d667d8d8e6a0
Jan Horvath <xhorvat6@nymfe10.fi.muni.cz>
2019-05-28 10:14:12 +0200

    Small changes to task 12

--- framework/labyrinth.hpp
+++ framework/labyrinth.hpp
@@ -9,8 +9,28 @@
 using namespace std;
 
 namespace Labyrinth {
+    class Graph {
+    public:
+
+        void setStart(int X, int Y, int Z) {
+            assert(X < m_cells.size() && Y < m_cells[X].size() && Z < m_cells[X][Y].size());
+            m_start = &m_cells[X][Y][Z];
+        }
+
+        void setTarget(int X, int Y, int Z) {
+            assert(X < m_cells.size() && Y < m_cells[X].size() && Z < m_cells[X][Y].size());
+            m_target = &m_cells[X][Y][Z];
+        }
+
+    private:
+        vector<vector<vector<Cell>>> m_cells;
+        Cell* m_start;
+        Cell* m_target;
+    };
+
     struct Cell {
-        vector<Cell*> neighbours; //pair<Box*, value>
+        vector<Cell*> m_neighbours;
+        //int value;
     };
 
     /*class LabyrinthStructure {
@@ -24,7 +44,7 @@ namespace Labyrinth {
         Labyrinth(unsigned X, unsigned Y, bool full = true) {
             if (full) {
                 //_cells =
-                auto x = vector<Cell>(Y, vector<Cell>(X, Cell(4, {nullptr, nullptr, nullptr, nullptr})));
+                auto x = vector<Cell>(Y, vector<Cell>(X, {nullptr, nullptr, nullptr, nullptr}));
             }
         }
     private:
